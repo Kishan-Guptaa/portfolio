d6aa550a4ecbf52164ec69c2993bb6f7e9d8dfa7
Kishan-Guptaa <kishangupta.code@gmail.com>
2025-07-24 21:32:09 +0530

    Update deploy scripts and vite config for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // src/App.jsx
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Hero from './components/Hero';
 import About from './components/About';
